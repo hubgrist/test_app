daefa6a29a89d1d74ed4fb73a220bca4db0d87a6
grist <wmacaulay@wilshire.com>
2013-06-25 08:17:23 +1000

    Turned off crfl warnings

---
+++ Users/wmacaulay/My Documents/ruby/test_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = '0b9af7fdc56bd33e132ac981a598f09980833fd897ff98b5f8b83369c217669e4e18c3e0866c6ea65473568b332b591401535432dee808975f8acd6e57d00af5'
